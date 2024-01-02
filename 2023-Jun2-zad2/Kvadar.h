8828e94919e6df6e68e107fc5ef9bc6eb2edc547
Aleksandar Gospavić <151999326+gospaleks@users.noreply.github.com>
2024-01-02 11:54:31 +0100

    2023-Jun2-zad2

---
+++ 2023-Jun2-zad2/Kvadar.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+using namespace std;
+class Kvadar
+{
+private:
+	float duzina;
+	float visina;
+	float sirina;
+	float gustina;
+
+	float getMasa();
+public:
+	Kvadar();
+	Kvadar(float d, float v, float s, float g);
+	~Kvadar() { }
+
+	friend ostream& operator<<(ostream& izlaz, Kvadar& k);
+	friend istream& operator>>(istream& ulaz, Kvadar& k);
+	bool operator>(Kvadar& k);
+	friend float operator/(Kvadar& k, float d);
+	friend float operator+(float l, Kvadar& k);
+};
+
